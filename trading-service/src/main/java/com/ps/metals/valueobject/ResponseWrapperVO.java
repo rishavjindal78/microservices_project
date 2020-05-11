5b85a415a0c755d1775a760093b3d7eda4f21ec1
Rishav Jindal <risjinda1@publicisgroupe.net>
2020-05-11 16:40:09 +0530

    fixed feign client error

--- trading-service/src/main/java/com/ps/metals/valueobject/ResponseWrapperVO.java
+++ trading-service/src/main/java/com/ps/metals/valueobject/ResponseWrapperVO.java
@@ -1,35 +1,25 @@
 package com.ps.metals.valueobject;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class ResponseWrapperVO {
 	   private Boolean success;
 	    private Boolean failure;
 	    private Object data;
 	    private String errorMessage;
 	    
-	    public ResponseWrapperVO() {
-	    	
-	    }
+	  	    
+	    @JsonCreator
+	    public ResponseWrapperVO(@JsonProperty("success") Boolean success, @JsonProperty("data") Object data, @JsonProperty("failure") Boolean failure,@JsonProperty("errorMessage") String errorMessage){
+	    	this.success = success;
+	    	this.failure = failure;
+	    	this.data = data;
+	    	this.errorMessage = errorMessage;
+	    } 
+
+
 	    
-
-	    public ResponseWrapperVO(Boolean success, Object data) {
-	        this.success = success;
-	        this.data = data;
-	    }
-
-	    public ResponseWrapperVO(Boolean failure, String errorMessage) {
-	        this.failure = failure;
-	        this.errorMessage = errorMessage;
-	    }
-
-
-	    public static ResponseWrapperVO success(Object data) {
-	        return new ResponseWrapperVO(true, data);
-	    }
-
-	    public static ResponseWrapperVO failure(String message) {
-	        return new ResponseWrapperVO(true, message);
-	    }
-
 	    public Boolean getSuccess() {
 	        return success;
 	    }
